6bc17a9e46a86ac91dcd3234e1b9ec56d06f950a
李光浩 <liguanghao@viatomtech.com>
2021-06-30 15:03:32 +0800

    yuyjyj

--- app/src/main/java/com/vaca/goodsocket/MainActivity.kt
+++ app/src/main/java/com/vaca/goodsocket/MainActivity.kt
@@ -2,10 +2,15 @@ package com.vaca.goodsocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.socket.client.IO
 
 class MainActivity : AppCompatActivity() {
+
+    val mSocket= IO.socket("http://192.168.5.100:3000")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mSocket.connect()
     }
 }
